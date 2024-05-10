20830e089235488fabeb2263478d8ceb0bbc4dc9
BHAWNA <149580291+AnudipCMSproject@users.noreply.github.com>
2024-05-10 07:58:46 +0530

    Add files via upload

---
+++ Employee-Management-System-FrontEnd/src/app/auth.guard.ts
@@ -0,0 +1,22 @@
+import { inject } from '@angular/core';
+import { CanActivateFn ,Router} from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const _router=inject(Router);
+  let isloggedIn=sessionStorage.getItem("isloggedIn");
+ if(isloggedIn='true'){
+  alert("login successful.....");
+    return true;
+   }
+
+//else{
+ // alert("login unsuccessful please try.....");
+ // _router.navigate(['login']);
+ // return true;
+//}
+
+   
+
+ 
+  return false;
+};
