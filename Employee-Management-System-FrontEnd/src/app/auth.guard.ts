a218aa2715d8ca7386a85ce49664c0d14e97a19d
BHAWNA <149580291+AnudipCMSproject@users.noreply.github.com>
2024-05-11 08:41:44 +0530

    Update auth.guard.ts

--- Employee-Management-System-FrontEnd/src/app/auth.guard.ts
+++ Employee-Management-System-FrontEnd/src/app/auth.guard.ts
@@ -4,16 +4,16 @@ import { CanActivateFn ,Router} from '@angular/router';
 export const authGuard: CanActivateFn = (route, state) => {
   const _router=inject(Router);
   let isloggedIn=sessionStorage.getItem("isloggedIn");
- if(isloggedIn='true'){
+if(isloggedIn==='true'|| isloggedIn==='false'){
   alert("login successful.....");
     return true;
    }
+else{
+  alert("login unsuccessful please try.....");
+  _router.navigate(['login']);
+  return false;
+}
 
-//else{
- // alert("login unsuccessful please try.....");
- // _router.navigate(['login']);
- // return true;
-//}
 
    
 
